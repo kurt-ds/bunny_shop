d20b5f93fe3f08f52f928cab938ad5da21bcf0c8
Kurt DS <allenkurtds.dev@gmail.com>
2026-03-17 09:58:02 +0800

    debug: removed inertia.js

---
+++ resources/js/Components/BunnyList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-for="bunny in bunnies" :key="bunny.id" >
+            <h2>{{ bunny.name  }}</h2>
+            <div v-html="bunny.description" ></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted} from 'vue';
+import axios from 'axios';
+
+
+const bunnies = ref([]);
+
+
+onMounted(async() => {
+    const response = await axios.get('/api/bunnies');
+
+    bunnies.value = response.data.data;
+});
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,11 @@
-import '../css/app.css';
-import './bootstrap';
+import './bootstrap'; // This loads Axios automatically in Laravel
+import { createApp } from 'vue';
+import BunnyList from './components/BunnyList.vue';
 
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+const app = createApp({});
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+// Register your component
+app.component('bunny-list', BunnyList);
 
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
-        ),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+// Mount it to the div with id="app"
+app.mount('#app');
