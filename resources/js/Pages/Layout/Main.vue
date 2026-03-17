3cc1ca4a85cee3d57db9baf13d68cc6db9deaa2d
Kurt DS <allenkurtds.dev@gmail.com>
2026-03-17 13:23:48 +0800

    feat: implemented footer

--- resources/js/Pages/Layout/Main.vue
+++ resources/js/Pages/Layout/Main.vue
@@ -5,9 +5,12 @@
         <main class="flex-grow container mx-auto p-6">
             <slot />
         </main>
+
+        <Footer />
     </div>
 </template>
 
 <script setup>
 import NavBar from "@/Components/NavBar.vue";
+import Footer from "@/Components/Footer.vue";
 </script>
